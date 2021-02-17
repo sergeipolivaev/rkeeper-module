037bfa7c6fc88cc1da91b243844bcdb313ff1728
AlexanderKornev <gonkamail.ru@mail.ru>
2021-02-18 00:58:29 +0500

    Fix sync

--- sync/index.js
+++ sync/index.js
@@ -6,12 +6,14 @@ const execAsync = promisify(exec);
 async function sync() {
   const options = { windowsHide: true };
   const res = await execAsync(`git pull`, options);
-  if (!res || res.stderr) {
+  if ((!res || res.stderr) && !res.stderr.includes("production -> origin/production")) {
     console.log(`error => ${ res.stderr }`);
     return false;
   }
 
-  if (!res.stdout.includes("Already up to date")) {
+  if (!res.stdout.includes("Already up to date") && 
+      res.stderr.includes("production -> origin/production")
+    ) {
     console.log();
     console.log("Update app");
     console.log();
