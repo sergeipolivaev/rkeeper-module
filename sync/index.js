09aefc1ec047ae842e4b04168a715ce142f5406a
AlexanderKornev <gonkamail.ru@mail.ru>
2021-02-19 00:17:11 +0500

    Change reload

--- sync/index.js
+++ sync/index.js
@@ -17,7 +17,7 @@ async function sync() {
     console.log();
     console.log("Update app");
     console.log();
-    await execAsync("start cmd /c reload.bat", options);
+    await execAsync("start cmd /k reload.bat", options);
     return true;
   }
 
