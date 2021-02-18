342adbfe02e914a04bdfb91406cbf86465571f09
AlexanderKornev <gonkamail.ru@mail.ru>
2021-02-18 23:28:00 +0500

    Change reload

--- reload.bat
+++ reload.bat
@@ -1,7 +1,4 @@
 start /b cmd /c npm install
-timeout 20 > NUL
 start /b cmd /c pm2 delete all
-timeout 20 > NUL
 start /b cmd /c pm2 start ecosystem.config.js
-timeout 20 > NUL
 start /b cmd /c pm2 save
