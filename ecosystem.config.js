ec279e62c401e464fb447e9d1cc5ffe9953945fe
AlexanderKornev <gonkamail.ru@mail.ru>
2021-04-28 17:05:19 +0500

    Add logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,11 @@ module.exports = {
     name: "rkeeper-module",
     script: "./app.js",
     watch: false,
-    instances  : 1,
-    exec_mode: "cluster"
+    instances: 1,
+    exec_mode: "cluster",
+    error_file: "./logs/err.log",
+    out_file: "./logs/out.log",
+    log_file: "./logs/combined.log",
+    time: true
   }]
 };
